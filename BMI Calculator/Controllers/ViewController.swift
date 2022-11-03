73dedfe4c33c512ec7f7e43eaa1eb74faac3be1b
akash.a.singh <113594078+arduino-akash@users.noreply.github.com>
2022-11-03 11:10:49 +0530

    Pushed all files

---
+++ BMI Calculator/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Singh, Akash | RIEPL on 03/11/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
